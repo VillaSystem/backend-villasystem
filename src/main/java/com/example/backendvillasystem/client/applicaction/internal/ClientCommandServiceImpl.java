b9fe22f773496f96c96ce2aecf4d285d36e95b28
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:25:34 -0500

    feat(clients): Client Command Service Implementation added.

--- src/main/java/com/example/backendvillasystem/client/applicaction/internal/ClientCommandServiceImpl.java
+++ src/main/java/com/example/backendvillasystem/client/applicaction/internal/ClientCommandServiceImpl.java
@@ -1,4 +1,38 @@
 package com.example.backendvillasystem.client.applicaction.internal;
 
-public class ClientCommandServiceImpl {
+import com.example.backendvillasystem.client.domain.model.aggregates.Clients;
+import com.example.backendvillasystem.client.domain.model.commands.CreateClientCommand;
+import com.example.backendvillasystem.client.domain.services.ClientCommandService;
+import com.example.backendvillasystem.client.infrastructure.persistence.jpa.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClientCommandServiceImpl implements ClientCommandService {
+    private final ClientRepository clientRepository;
+
+    public ClientCommandServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    /**
+     * Handle the create client command
+     *
+     * @param command The create client command
+     * @return The created client
+     * @throws IllegalArgumentException If the client already exists
+     * @see CreateClientCommand
+     */
+    @Override
+    public Optional<Clients> handle(CreateClientCommand command) {
+        if (clientRepository.existsByEmailOrDni(command.email(), command.dni())) {
+            throw new IllegalArgumentException("Client with this email or DNI already exists");
+        }
+
+        var clients = new Clients(command);
+        var createdClient = clientRepository.save(clients);
+        return Optional.of(createdClient);
+    }
+
 }
