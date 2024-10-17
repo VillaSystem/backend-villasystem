9383bb7787801ef6d712cd2c84ae7e98f5f2b0c6
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:28:32 -0500

    feat(clients): Client Repository added.

--- src/main/java/com/example/backendvillasystem/client/infrastructure/persistence/jpa/ClientRepository.java
+++ src/main/java/com/example/backendvillasystem/client/infrastructure/persistence/jpa/ClientRepository.java
@@ -1,4 +1,24 @@
 package com.example.backendvillasystem.client.infrastructure.persistence.jpa;
 
-public class ClientRepository {
+import com.example.backendvillasystem.client.domain.model.aggregates.Clients;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * JPA Repository for the Clients entity
+ * @summary
+ * This interface extends the JpaRepository to provide CRUD operations for the Clients entity
+ * It extends Spring Data JpaRepository with Clients as the entity type
+ * and Long as the ID type
+ * @since 1.0
+ */
+@Repository
+public interface ClientRepository extends JpaRepository<Clients, Long> {
+    /**
+     * Check if a client exists by email or DNI
+     * @param email The email of the client
+     * @param dni The DNI of the client
+     * @return True if a client with the given email or DNI exists, false otherwise
+     */
+    boolean existsByEmailOrDni(String email, String dni);
 }
