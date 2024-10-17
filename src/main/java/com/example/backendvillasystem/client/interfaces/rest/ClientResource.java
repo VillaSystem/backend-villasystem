6d80a268b64be327fde0dff1e253c68a868c70a9
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:28:46 -0500

    feat(clients): Client Resource added.

--- src/main/java/com/example/backendvillasystem/client/interfaces/rest/ClientResource.java
+++ src/main/java/com/example/backendvillasystem/client/interfaces/rest/ClientResource.java
@@ -1,4 +1,10 @@
 package com.example.backendvillasystem.client.interfaces.rest;
 
-public record ClientResource() {
+public record ClientResource(Long id, String firstName, String lastName, String phone, String address, String country,
+                              String city, String dni, String email, String password) {
+
 }
+
+
+
+
