1599ede048a17ad6218fd25d7971916aa8d05380
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:26:32 -0500

    feat(clients): Client Query service implementation added.

--- src/main/java/com/example/backendvillasystem/client/applicaction/internal/ClientQueryServiceImpl.java
+++ src/main/java/com/example/backendvillasystem/client/applicaction/internal/ClientQueryServiceImpl.java
@@ -1,4 +1,23 @@
 package com.example.backendvillasystem.client.applicaction.internal;
 
-public class ClientQueryServiceImpl {
+import com.example.backendvillasystem.client.domain.model.aggregates.Clients;
+import com.example.backendvillasystem.client.domain.model.queries.GetClientsByIdQuery;
+import com.example.backendvillasystem.client.domain.services.ClientQueryService;
+import com.example.backendvillasystem.client.infrastructure.persistence.jpa.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClientQueryServiceImpl implements ClientQueryService {
+    private final ClientRepository clientRepository;
+
+    public ClientQueryServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public Optional<Clients> handle(GetClientsByIdQuery query) {
+        return clientRepository.findById(query.id());
+    }
 }
