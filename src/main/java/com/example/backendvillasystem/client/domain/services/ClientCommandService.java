90fb7beda0f185c55bc81570ecbd6a3905b07545
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:25:06 -0500

    feat(clients): Client Command Service added.

--- src/main/java/com/example/backendvillasystem/client/domain/services/ClientCommandService.java
+++ src/main/java/com/example/backendvillasystem/client/domain/services/ClientCommandService.java
@@ -1,4 +1,24 @@
 package com.example.backendvillasystem.client.domain.services;
 
+
+import com.example.backendvillasystem.client.domain.model.aggregates.Clients;
+import com.example.backendvillasystem.client.domain.model.commands.CreateClientCommand;
+
+import java.util.Optional;
+
+/**
+ * @name ClientCommandService
+ * @summary
+ * This interface represents the service that handles the client commands
+ */
 public interface ClientCommandService {
+    /**
+     * Handle the create client command
+     * @param command The create client command
+     * @return The created client
+     *
+     * @throws IllegalArgumentException If ClientApiKey is null or empty
+     * @see CreateClientCommand
+     */
+    Optional<Clients> handle(CreateClientCommand command);
 }
