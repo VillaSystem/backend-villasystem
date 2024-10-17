40e044cbb97d7af6f502cbcd822e3c6235f8becf
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:29:03 -0500

    feat(clients): Client Resource From Entity Assembler added.

--- src/main/java/com/example/backendvillasystem/client/interfaces/rest/ClientResourceFromEntityAssembler.java
+++ src/main/java/com/example/backendvillasystem/client/interfaces/rest/ClientResourceFromEntityAssembler.java
@@ -1,5 +1,21 @@
 package com.example.backendvillasystem.client.interfaces.rest;
 
-public class ClientSourceResourceFromEntityAssembler {
-    public static Client
+import com.example.backendvillasystem.client.domain.model.aggregates.Clients;
+
+public class ClientResourceFromEntityAssembler {
+    public static ClientResource toResourceFromEntity(Clients entity) {
+        return new ClientResource(
+                entity.getId(),
+                entity.getFirstName(),
+                entity.getLastName(),
+                entity.getPhone(),
+                entity.getAddress(),
+                entity.getCountry(),
+                entity.getCity(),
+                entity.getDni(),
+                entity.getEmail(),
+                entity.getPassword()
+
+        );
+    }
 }
