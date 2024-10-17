234b0faae75b559d54ff9c9714c021a36ac6de97
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:26:03 -0500

    feat(clients): Client Query service added.

--- src/main/java/com/example/backendvillasystem/client/domain/services/ClientQueryService.java
+++ src/main/java/com/example/backendvillasystem/client/domain/services/ClientQueryService.java
@@ -1,4 +1,24 @@
 package com.example.backendvillasystem.client.domain.services;
 
+import com.example.backendvillasystem.client.domain.model.aggregates.Clients;
+import com.example.backendvillasystem.client.domain.model.queries.GetClientsByIdQuery;
+
+import java.util.Optional;
+
+/**
+ * @name ClientQueryService
+ * @summary
+ * This interface represents the service that handles the client queries
+ */
 public interface ClientQueryService {
+
+    /**
+     * Handle the get clients by id query
+     * @param query The get clients by id query
+     * @return The client
+     *
+     * @throws IllegalArgumentException If id is null
+     * @see GetClientsByIdQuery
+     */
+    Optional<Clients> handle(GetClientsByIdQuery query);
 }
