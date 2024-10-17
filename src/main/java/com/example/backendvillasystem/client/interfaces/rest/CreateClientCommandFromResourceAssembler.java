b6f12c47578bf8003e02179acfb470f6f194fba8
petitavo <Gustavohuanca1020@gmail.com>
2024-10-17 18:29:54 -0500

    feat(clients): Create Client Command From Resource Assembler added.

--- src/main/java/com/example/backendvillasystem/client/interfaces/rest/CreateClientCommandFromResourceAssembler.java
+++ src/main/java/com/example/backendvillasystem/client/interfaces/rest/CreateClientCommandFromResourceAssembler.java
@@ -1,4 +1,21 @@
 package com.example.backendvillasystem.client.interfaces.rest;
 
+import com.example.backendvillasystem.client.domain.model.commands.CreateClientCommand;
+
 public class CreateClientCommandFromResourceAssembler {
+
+    public static CreateClientCommand toCommandFromResource(CreateClientResource resource) {
+        return new CreateClientCommand(
+                resource.firstName(),
+                resource.lastName(),
+                resource.phone(),
+                resource.address(),
+                resource.country(),
+                resource.city(),
+                resource.dni(),
+                resource.email(),
+                resource.password(),
+                resource.role()
+        );
+    }
 }
