3720687ec008dd94d4fd5e7b3504e18477567fce
petitavo <Gustavohuanca1020@gmail.com>
2024-10-18 12:21:59 -0500

    feat(clients): get all clients added.

--- src/main/java/com/example/backendvillasystem/client/applicaction/internal/ClientQueryServiceImpl.java
+++ src/main/java/com/example/backendvillasystem/client/applicaction/internal/ClientQueryServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.backendvillasystem.client.domain.services.ClientQueryService;
 import com.example.backendvillasystem.client.infrastructure.persistence.jpa.ClientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,8 +17,26 @@ public class ClientQueryServiceImpl implements ClientQueryService {
         this.clientRepository = clientRepository;
     }
 
+    /**
+     * Handles the GetClientsByIdQuery query.
+     *
+     * @param query The get clients by id query.
+     * @return The client if exists.
+     * @throws IllegalArgumentException If id is null.
+     * @see GetClientsByIdQuery
+     */
     @Override
     public Optional<Clients> handle(GetClientsByIdQuery query) {
         return clientRepository.findById(query.id());
+    }
+
+    /**
+     * Retrieves all clients.
+     *
+     * @return List of all clients.
+     */
+    @Override
+    public List<Clients> getAllClients() {
+        return clientRepository.findAll();
     }
 }
