069142b6e6b81913c5a2dae79d19f34b9ef26633
AngeloC12 <angelocuri@hotmail.com>
2024-10-20 16:47:15 -0500

    feat: create InventoryCommandService,Controller,QueryService

---
+++ src/main/java/com/example/backendvillasystem/inventory/applicaction/internal/InventoryQueryServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.backendvillasystem.inventory.applicaction.internal;
+
+import com.example.backendvillasystem.inventory.domain.model.aggregates.Inventories;
+import com.example.backendvillasystem.inventory.domain.model.queries.GetInventoriesByIdQuery;
+import com.example.backendvillasystem.inventory.domain.services.InventoryQueryService;
+import com.example.backendvillasystem.inventory.infrastructure.presistence.jpa.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InventoryQueryServiceImpl implements InventoryQueryService {
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryQueryServiceImpl(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    /**
+     * Handles the GetInventoriesByIdQuery query.
+     *
+     * @param query The get inventories by id query.
+     * @return The inventory item if exists.
+     * @throws IllegalArgumentException If id is null.
+     * @see GetInventoriesByIdQuery
+     */
+    @Override
+    public Optional<Inventories> handle(GetInventoriesByIdQuery query) {
+        return inventoryRepository.findById(query.id());
+    }
+
+    /**
+     * Retrieves all inventory items.
+     *
+     * @return List of all inventory items.
+     */
+    @Override
+    public List<Inventories> getAllInventories() {
+        return inventoryRepository.findAll();
+    }
+}
