7b0989394af290a3af70dd73050dee0a38cecbff
petitavo <Gustavohuanca1020@gmail.com>
2024-10-25 20:16:23 -0500

    chore: http added.

--- src/main/java/com/example/backendvillasystem/shared/OpenApiConfiguration.java
+++ src/main/java/com/example/backendvillasystem/shared/OpenApiConfiguration.java
@@ -1,9 +1,9 @@
 package com.example.backendvillasystem.shared;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +13,11 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI villaSystemOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://backend-villasystem-production-36d5.up.railway.app")) // URL con HTTPS
                 .info(new Info()
                         .title("Villa System REST API Documentation")
                         .description("This is the REST API documentation for the Villa System application using springdoc-openapi and OpenAPI 3.")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0")
-                                .url("https://opensource.org")));
+                        .license(new License().name("Apache 2.0").url("https://opensource.org")));
     }
 }
