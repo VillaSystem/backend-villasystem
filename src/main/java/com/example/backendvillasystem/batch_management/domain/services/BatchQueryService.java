72c9d72bd3d7d062139b04599b2354a05e1d6574
Racso24k <u20211g192@upc.edu.pe>
2024-10-28 20:31:37 -0500

    feat(Batch): Batch Query Service update.

--- src/main/java/com/example/backendvillasystem/batch_management/domain/services/BatchQueryService.java
+++ src/main/java/com/example/backendvillasystem/batch_management/domain/services/BatchQueryService.java
@@ -2,13 +2,14 @@ package com.example.backendvillasystem.batch_management.domain.services;
 
 import com.example.backendvillasystem.batch_management.domain.model.aggregates.Batch;
 import com.example.backendvillasystem.batch_management.domain.model.queries.GetBatchesByIdQuery;
+import com.example.backendvillasystem.batch_management.domain.model.queries.GetBatchesByProducerIdQuery;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BatchQueryService {
     Optional<Batch> handle(GetBatchesByIdQuery query);
-
-    List<com.example.backendvillasystem.batch_management.domain.model.aggregates.Batch> getAllBatches();
+    List<Batch> handle(GetBatchesByProducerIdQuery query);
+    List<Batch> getAllBatches();
 
 }
