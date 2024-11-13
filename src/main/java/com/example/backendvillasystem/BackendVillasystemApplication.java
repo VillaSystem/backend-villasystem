05b70e37c088b3185ba1df1fbb10ae75c32a9f95
petitavo <gustavohuanca1020@gmail.com>
2024-11-13 18:18:13 -0500

    feat(client-management): cors.

--- src/main/java/com/example/backendvillasystem/BackendVillasystemApplication.java
+++ src/main/java/com/example/backendvillasystem/BackendVillasystemApplication.java
@@ -3,6 +3,9 @@ package com.example.backendvillasystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +15,18 @@ public class BackendVillasystemApplication {
         SpringApplication.run(BackendVillasystemApplication.class, args);
     }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
