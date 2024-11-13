e9fdaaf8f9ec1ce24b27ae51d7f7882f047b5145
petitavo <gustavohuanca1020@gmail.com>
2024-11-13 18:42:20 -0500

    feat(client-management): cors.

--- src/main/java/com/example/backendvillasystem/BackendVillasystemApplication.java
+++ src/main/java/com/example/backendvillasystem/BackendVillasystemApplication.java
@@ -22,7 +22,7 @@ public class BackendVillasystemApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://backend-villasystem-production-36d5.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
