4e000c4491cb7a9f40a0ab6ee550650f7fcdaba4
AngeloC12 <angelocuri@hotmail.com>
2024-11-14 00:09:55 -0500

    feat: create UpdateInventoryResource

---
+++ src/main/java/com/example/backendvillasystem/inventory_management/interfaces/rest/resources/UpdateInventoryResource.java
@@ -0,0 +1,28 @@
+package com.example.backendvillasystem.inventory_management.interfaces.rest.resources;
+
+public record UpdateInventoryResource(String name, String type, String unit, String expirationDate,
+                                      String supplier, Double unitCost, int quantity, String lastUpdated, Long producerId) {
+    public UpdateInventoryResource {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (type == null || type.isBlank()) {
+            throw new IllegalArgumentException("Type cannot be null or empty");
+        }
+        if (unit == null || unit.isBlank()) {
+            throw new IllegalArgumentException("Unit cannot be null or empty");
+        }
+        if (supplier == null || supplier.isBlank()) {
+            throw new IllegalArgumentException("Supplier cannot be null or empty");
+        }
+        if (unitCost == null || unitCost <= 0) {
+            throw new IllegalArgumentException("Unit cost cannot be null or less than or equal to 0");
+        }
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative");
+        }
+        if (producerId == null || producerId <= 0) {
+            throw new IllegalArgumentException("Producer ID cannot be null or less than 1");
+        }
+    }
+}
