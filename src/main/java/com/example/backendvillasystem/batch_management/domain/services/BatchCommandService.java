0d1843c6c4942f398dbdedfccec9e805220becae
Racso24k <u20211g192@upc.edu.pe>
2024-11-15 23:06:00 -0500

    feat(Batch): Create Update Batch Command Service.

--- src/main/java/com/example/backendvillasystem/batch_management/domain/services/BatchCommandService.java
+++ src/main/java/com/example/backendvillasystem/batch_management/domain/services/BatchCommandService.java
@@ -2,10 +2,16 @@ package com.example.backendvillasystem.batch_management.domain.services;
 
 import com.example.backendvillasystem.batch_management.domain.model.aggregates.Batch;
 import com.example.backendvillasystem.batch_management.domain.model.commands.CreateBatchCommand;
+import com.example.backendvillasystem.batch_management.domain.model.commands.DeleteBatchCommand;
+import com.example.backendvillasystem.batch_management.domain.model.commands.UpdateBatchCommand;
 
 import java.util.Optional;
 
 public interface BatchCommandService {
     Optional<Batch> handle(CreateBatchCommand command);
+
+    Optional<Batch> handle(UpdateBatchCommand command);
+
+    void handle(DeleteBatchCommand command);
 }
 
